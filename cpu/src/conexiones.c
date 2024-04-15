32d84df8f860abce9ba423ef23183ed8986381b4
Lucas Ocampo <lucocampo@frba.utn.edu.ar>
2024-04-15 19:21:44 +0000

    conexiones cpu

---
+++ cpu/src/conexiones.c
@@ -0,0 +1,41 @@
+#include <conexiones.h>
+
+int escucharConexiones(void) {
+
+    if (nuevo_config == NULL) {
+        exit(1);
+    }; 
+
+    char* puertodispatch = config_get_string_value(nuevo_config, "PUERTO_ESCUCHA_DISPATCH");
+    char* puertointerrupt = config_get_string_value(nuevo_config, "PUERTO_ESCUCHA_INTERRUPT");
+
+	logger = log_create("ServidorCpu.log", "Servidor Cpu", 1, LOG_LEVEL_DEBUG);
+
+    int server_di = iniciar_servidor(puertodispatch);
+    int server_it = iniciar_servidor(puertointerrupt);
+
+	log_info(logger, "Servidor listo para recibir al cliente");
+
+    esperar_cliente(server_di);
+	esperar_cliente(server_it);
+
+    t_config* nuevo_config = config_create("cpu.config");
+    
+    // Dispatch
+    pthread_t thread;
+    pthread_create(&thread,
+						NULL,
+						(void*) esperar_cliente,
+						server_di);
+	pthread_detach(thread);
+    
+    // Interrupt
+    pthread_t thread2;
+    pthread_create(&thread2,
+						NULL,
+						(void*) esperar_cliente,
+						server_it);
+	pthread_detach(thread2);
+
+	return 0;
+}
