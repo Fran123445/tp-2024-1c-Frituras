abf2c14fecaa252a19be7d9cff7f0b5606be7b8c
Franco Stazzone <fralstazzone@gmail.com>
2024-05-06 17:58:37 +0000

    Mueve los registros de la CPU a utils

--- cpu/src/registroCpu.h
+++ cpu/src/registroCpu.h
@@ -2,31 +2,8 @@
 #define REGISTROCPU_H_
 
 #include <stdio.h>
-#include <stdint.h> // Incluye esta línea si utilizas tipos de datos como uint8_t o uint32_t
 #include <stddef.h>
-
-// Declaraciones de tipos de datos
-typedef enum {
-    AX,
-    BX,
-    CX,
-    DX,
-    EAX,
-    EBX,
-    ECX,
-    EDX,
-    SI,
-    DI,
-    PC
-} registrosCPU;
-
-typedef struct {
-    // Definición de los registros de la CPU
-    uint8_t AX, BX, CX, DX;
-    uint32_t EAX, EBX, ECX, EDX;
-    uint32_t SI, DI, PC;
-} registros_cpu;
-
+#include <utils/pcb.h>
 
 // Declaraciones de variables globales externas
 extern registros_cpu miCPU;
