2dd8cae11303e8451a975e3ae34df2ef380500d7
Franco Stazzone <fralstazzone@gmail.com>
2024-05-31 22:17:47 +0000

    Agrega una funcion para enviar un proceso a la cola prioritaria de VRR

--- kernel/src/virtualRoundRobin.h
+++ kernel/src/virtualRoundRobin.h
@@ -7,7 +7,9 @@
 #include <commons/temporal.h>
 #include <commons/collections/queue.h>
 #include <utils/pcb.h>
+#include <roundRobin.h>
 
 extern t_queue* colaPrioritaria;
+extern pthread_mutex_t mutexColaPrioritaria;
 
 #endif /* VRR_H */
--- kernel/src/virtualRounrdRobin.c
+++ kernel/src/virtualRounrdRobin.c
@@ -1,1 +1,18 @@
 #include <virtualRoundRobin.h>
+
+// indica que si el ultimo proceso enviado salio o no de la cola prioritaria
+int ultimoPrioritario = 0;
+
+int pidProcesoEnEjecucion;
+
+void enviarAColaPrioritaria(PCB* proceso) {
+    pthread_mutex_lock(&mutexColaPrioritaria);
+    cambiarEstado(proceso, ESTADO_READY);
+    queue_push(colaPrioritaria, proceso);
+    pthread_mutex_unlock(&mutexColaPrioritaria);
+
+    if (!ultimoPrioritario) {
+        enviarInterrupcion(pidProcesoEnEjecucion);
+        ultimoPrioritario = 1;
+    }
+}
