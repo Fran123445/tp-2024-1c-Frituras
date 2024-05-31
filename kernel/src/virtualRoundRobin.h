f1afa8470d5953ac8f4a21ec39c56ccc6af8762b
Franco Stazzone <fralstazzone@gmail.com>
2024-05-31 21:48:54 +0000

    Crea archivos relacionados a VRR

---
+++ kernel/src/virtualRoundRobin.h
@@ -0,0 +1,13 @@
+#ifndef VRR_H
+#define VRR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <commons/temporal.h>
+#include <commons/collections/queue.h>
+#include <utils/pcb.h>
+
+extern t_queue* colaPrioritaria;
+
+#endif /* VRR_H */
