d7b512eb5dce3e8e9a681a0d3a219f13bc0e9cd5
Yanel Agostini Dohmen <yagostinidohmen@frba.utn.edu.ar>
2024-06-07 16:39:47 +0000

    Avances sobre creación y eliminación de proceso con Kernel

--- memoria/src/memoriaDeInstrucciones.c
+++ memoria/src/memoriaDeInstrucciones.c
@@ -2,7 +2,7 @@
 
 pthread_mutex_t mutex_lista_de_procesos_con_ins = PTHREAD_MUTEX_INITIALIZER;
 
-t_proceso* hallar_proceso(int PID){
+t_proceso* hallar_proceso_lista_ins(int PID){
     bool _mismoPID(t_proceso* proceso){
         return (proceso->pid == PID);
     };
@@ -12,6 +12,20 @@ t_proceso* hallar_proceso(int PID){
     pthread_mutex_unlock(&mutex_lista_de_procesos_con_ins);
 
     return encontrado;
+}
+void destruir_proceso(t_proceso* proceso){
+    if(proceso!=NULL){
+        list_destroy_and_destroy_elements(proceso->instrucciones,free); // libera memoria asignada a cada instruccion del proceso y la lista proceso->instrucciones
+        free(proceso); //libera la memoria asignada al propio proceso
+    }
+}
+void eliminar_proceso_de_lista_ins(int PID){
+    bool _mismoPID(t_proceso* proceso){
+        return (proceso->pid == PID);
+    };
+    pthread_mutex_lock(&mutex_lista_de_procesos_con_ins);
+    list_remove_and_destroy_by_condition(lista_de_procesos_con_ins, (void*)_mismoPID, (void*) destruir_proceso);
+    pthread_mutex_unlock(&mutex_lista_de_procesos_con_ins);
 }
 
 char* obtener_instruccion(int socket_kernel, int pc, int pid){
