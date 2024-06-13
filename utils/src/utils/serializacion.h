724b6ac5646f530db410940abc4e62010643de69
Yanel Agostini Dohmen <129529032+yaanel@users.noreply.github.com>
2024-06-13 19:31:36 -0300

    Cambios en Buffer read y Creacion Bufer read tamaño

--- utils/src/utils/serializacion.h
+++ utils/src/utils/serializacion.h
@@ -31,13 +31,17 @@ typedef enum {
     CREACION_PROCESO, //Acá Kernel me envia path
     FIN_PROCESO,
     //CPU o IO - Memoria
-    ACCESO_ESPACIO_USUARIO,
+    ACCESO_ESPACIO_USUARIO_LECTURA,
+    ACCESO_ESPACIO_USUARIO_ESCRITURA,
+    ESCRITURA_REALIZADA_OK,
     CONEXION_DIAL_FS,
     //Memoria de Instrucciones - CPU
     ENVIO_DE_INSTRUCCIONES,
     // Cualquier módulo a Memoria
     AJUSTAR_SIZE_PROCESO,
     ACCESO_TABLAS_PAGINAS,
+    OUT_OF_MEMORY,
+    RESIZE_ACEPTADO,
 
 }op_code;
 
@@ -60,7 +64,7 @@ void enviar_paquete(t_paquete* paquete, int socket_cliente);
 t_buffer* recibir_buffer(int socket_cliente);
 void eliminar_paquete(t_paquete* paquete);
 int recibir_operacion(int socket_cliente);
-
+void agregar_uint32_a_paquete(t_paquete* paquete, uint32_t valor);
 void agregar_int_a_paquete(t_paquete* paquete, int valor);
 void agregar_string_a_paquete(t_paquete* paquete, char* string);
 void agregar_PCB_a_paquete(t_paquete* paquete, PCB* pcb);
@@ -68,7 +72,9 @@ void agregar_instruccion_a_paquete(t_paquete* paquete, t_instruccion* instruccio
 void agregar_interfaz_generica_a_paquete(t_paquete* paquete, t_interfaz_generica* interfaz);
 
 int buffer_read_int(t_buffer* buffer);
+void buffer_read(t_buffer* buffer, void* data);
 uint32_t buffer_read_uint32(t_buffer* buffer);
+int read_buffer_tamanio (t_buffer* buffer);
 char* buffer_read_string(t_buffer* buffer);
 PCB* buffer_read_pcb(t_buffer* buffer);
 t_instruccion* buffer_read_instruccion(t_buffer* buffer);
