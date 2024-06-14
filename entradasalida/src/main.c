307b5ff6b05227c6ee113344a7ab3e329a545ea0
Lucas Ocampo <lucocampo@frba.utn.edu.ar>
2024-06-14 04:14:50 +0000

    stdin y stdout updates

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -33,44 +33,55 @@ void iniciarInterfazGenerica(int socket, t_config* config, char* nombre){
 }
 
 void iniciarInterfazSTDIN(int socket, t_config* config, char* nombre) {
-
     char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
     char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
     int conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, IO);
 
-    while (1) {
-        ssize_t reciv = recibir_operacion(socket);
+    char* texto;
+    printf("Ingrese texto: ");
+    fgets(texto, sizeof(texto), stdin);
+    int texto_len = strlen(texto);
 
+    int enviado = 0;
+
+    while (enviado < texto_len) {
+        ssize_t reciv = recibir_operacion(socket);
         if (reciv < 0) {
             exit(-1);
         }
 
         t_buffer* buffer = recibir_buffer(socket);
-        int direccion_fisica = buffer_read_int(buffer);
+        uint32_t direccion_fisica = buffer_read_uint32(buffer);
+        uint32_t tam = buffer_read_uint32(buffer);
 
-        char* texto;
-        printf("Ingrese texto: ");
-        fgets(texto, sizeof(texto), stdin);
+        while (enviado < texto_len && tam > 0) {
+            int tam_envio = (texto_len - enviado > tam) ? tam : texto_len - enviado;
+            char* parte_texto = strndup(texto + enviado, tam_envio);
+        
+            enviar_a_memoria_para_escritura(direccion_fisica,parte_texto, tam_envio);
 
-        t_paquete* paquete = crear_paquete();
-        agregar_int_a_paquete(paquete, direccion_fisica);
-        agregar_string_a_paquete(paquete, texto);
-        enviar_paquete(paquete, conexion_memoria);
-        eliminar_paquete(paquete);
+            free(parte_texto);
+            enviado += tam_envio;
 
-        t_paquete* paquete_respuesta = crear_paquete();
-        paquete_respuesta->codigo_operacion = OPERACION_FINALIZADA;
-        enviar_paquete(paquete_respuesta, socket);
-        eliminar_paquete(paquete_respuesta);
+            direccion_fisica += tam_envio;
+
+            tam -= tam_envio;  // Reducimos el tamaño restante de la página
+        }
+
     }
+    t_paquete* paquete = crear_paquete(OPERACION_FINALIZADA);
+    enviar_paquete(paquete ,socket);
+    eliminar_paquete(paquete);
+
 }
 
-
 void iniciarInterfazSTDOUT(int socket, t_config* config, char* nombre) {
-
     char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
     char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
     int conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, IO);
+    
+    char* texto_completo = NULL;
+    int texto_completo_len = 0;
 
     while (1) {
         ssize_t reciv = recibir_operacion(socket);
@@ -80,26 +91,28 @@ void iniciarInterfazSTDOUT(int socket, t_config* config, char* nombre) {
         }
 
         t_buffer* buffer = recibir_buffer(socket);
-        int direccion_fisica = buffer_read_int(buffer);
+        uint32_t direccion_fisica = buffer_read_uint32(buffer);
+        uint32_t tam = buffer_read_uint32(buffer);
 
-        t_paquete* paquete_memoria = crear_paquete();
-        agregar_int_a_paquete(paquete_memoria, direccion_fisica);
-        enviar_paquete(paquete_memoria, conexion_memoria);
-        eliminar_paquete(paquete_memoria);
+        char* texto_pagina = (char*) contenido_obtenido_de_memoria(direccion_fisica, tam);
+            
+        // Concatenar la página recibida al texto completo
+        texto_completo = realloc(texto_completo, texto_completo_len + strlen(texto_pagina) + 1);
+        strcat(texto_completo, texto_pagina);
+        texto_completo_len += strlen(texto_pagina);
 
-        t_buffer* buffer_memoria = recibir_buffer(conexion_memoria);
-        char* texto = buffer_read_string(buffer_memoria);
+        // Liberar recursos
+        free(texto_pagina);
 
-        printf("STDOUT: %s\n", texto);
-        free(texto);
-        eliminar_buffer(buffer_memoria);
+        eliminar_buffer(buffer);
 
-    
-        t_paquete* paquete_respuesta = crear_paquete();
-        paquete_respuesta->codigo_operacion = OPERACION_FINALIZADA;
-        enviar_paquete(paquete_respuesta, socket);
-        eliminar_paquete(paquete_respuesta);
-    }
+    }   
+    printf("STDOUT: %s\n", texto_completo);
+    free(texto_completo);
+    t_paquete* paquete = crear_paquete(OPERACION_FINALIZADA);
+    enviar_paquete(paquete ,socket);
+    eliminar_paquete(paquete);
+
 }
 
 
