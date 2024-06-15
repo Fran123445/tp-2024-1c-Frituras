b7e84ca7385a74caa82a382696955c61e161192b
matuaponte <maponte@frba.utn.edu.ar>
2024-06-15 00:29:42 +0000

    Agrego funciones para las conexiones con memoria

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -4,6 +4,8 @@
 #include <utils/server.h>
 #include <utils/serializacion.h>
 
+int conexion_memoria;
+
 void iniciarInterfazGenerica(int socket, t_config* config, char* nombre){
 
     int tiempo_pausa = config_get_int_value(config, "TIEMPO_UNIDAD_TRABAJO");
@@ -27,16 +29,49 @@ void iniciarInterfazGenerica(int socket, t_config* config, char* nombre){
         t_paquete* paquete = crear_paquete(OPERACION_FINALIZADA);
         enviar_paquete(paquete ,socket);
         eliminar_paquete(paquete);
-
     }
+}
 
+void pedir_contenido_memoria(uint32_t direccion_fisica, uint32_t tam, uint32_t  pid){
+    t_paquete* paquete = crear_paquete(ACCESO_ESPACIO_USUARIO_LECTURA);
+    agregar_uint32_a_paquete(paquete, direccion_fisica);
+    agregar_uint32_a_paquete(paquete, tam);
+    agregar_uint32_a_paquete(paquete, pid);
+    enviar_paquete(paquete, conexion_memoria);
+    eliminar_paquete(paquete);
+}
+
+void* recibir_contenido_memoria(){
+    op_code cod_op = recibir_operacion(conexion_memoria);
+    if(cod_op == ACCESO_ESPACIO_USUARIO_LECTURA){
+        t_buffer* buffer = recibir_buffer(conexion_memoria);
+        void* data;
+        buffer_read(buffer, data);
+        liberar_buffer(buffer);
+        return data;
+    }
+    return NULL;
+}
+
+void* contenido_obtenido_de_memoria(uint32_t direccion_fisica, uint32_t tam, uint32_t  pid){
+    pedir_contenido_memoria(direccion_fisica, tam, pid);
+    void* contenido_leido = recibir_contenido_memoria();  //le pido a memoria el contenido de la pagina
+    // void* puntero_al_dato_leido = &contenido_leido; Me parece que esto ya no hace falta, veremos si hay seg. fault o no
+    return contenido_leido;
+}
+
+void enviar_a_memoria_para_escribir(uint32_t direccion_fisica, void* datos_a_escribir, uint32_t tam, int pid)  {
+    t_paquete* paquete = crear_paquete(ACCESO_ESPACIO_USUARIO_ESCRITURA);
+    agregar_uint32_a_paquete(paquete, direccion_fisica);
+    agregar_uint32_a_paquete(paquete, tam);
+    agregar_int_a_paquete(paquete,pid);
+    agregar_a_paquete(paquete, datos_a_escribir,tam);
+    enviar_paquete(paquete, conexion_memoria);
+    eliminar_paquete(paquete);
 }
 
 void iniciarInterfazSTDIN(int socket, t_config* config, char* nombre) {
-    char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
-    char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
-    int conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, IO);
-
+    
     char* texto;
     printf("Ingrese texto: ");
     if (fgets(texto, sizeof(texto), stdin) == NULL) {
@@ -61,12 +96,13 @@ void iniciarInterfazSTDIN(int socket, t_config* config, char* nombre) {
         t_buffer* buffer = recibir_buffer(socket);
         uint32_t direccion_fisica = buffer_read_uint32(buffer);
         uint32_t tam = buffer_read_uint32(buffer);
+        int pid = buffer_read_int(buffer);
 
         while (enviado < texto_len && tam > 0) {
             int tam_envio = (texto_len - enviado > tam) ? tam : texto_len - enviado;
             char* parte_texto = strndup(texto + enviado, tam_envio);
         
-            enviar_a_memoria_para_escritura(direccion_fisica,parte_texto, tam_envio);
+            enviar_a_memoria_para_escribir(direccion_fisica,parte_texto, tam_envio,pid);
 
             free(parte_texto);
             enviado += tam_envio;
@@ -84,10 +120,7 @@ void iniciarInterfazSTDIN(int socket, t_config* config, char* nombre) {
 }
 
 void iniciarInterfazSTDOUT(int socket, t_config* config, char* nombre) {
-    char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
-    char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
-    int conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, IO);
-    
+
     char* texto_completo = NULL;
 
     ssize_t reciv = recibir_operacion(socket);
@@ -98,10 +131,10 @@ void iniciarInterfazSTDOUT(int socket, t_config* config, char* nombre) {
     t_buffer* buffer = recibir_buffer(socket);
     uint32_t direccion_fisica = buffer_read_uint32(buffer);
     uint32_t tam = buffer_read_uint32(buffer);
+    uint32_t  pid = buffer_read_uint32(buffer);
 
-    texto_completo = (char*) contenido_obtenido_de_memoria(direccion_fisica, tam);
+    texto_completo = (char*) contenido_obtenido_de_memoria(direccion_fisica, tam, pid);
             
-
     printf("STDOUT: %s\n", texto_completo);
     free(texto_completo);
     
@@ -120,6 +153,7 @@ int main(int argc, char* argv[]) {
     }; 
 
     int conexion_kernel = crear_conexion(config_get_string_value(nuevo_config,"IP_KERNEL"), config_get_string_value(nuevo_config, "PUERTO_KERNEL"), IO);
+    conexion_memoria = crear_conexion(config_get_string_value(nuevo_config, "IP_MEMORIA"), config_get_string_value(nuevo_config, "PUERTO_MEMORIA"), IO);
 
     char* tipo = config_get_string_value(nuevo_config,"TIPO_INTERFAZ");
 
